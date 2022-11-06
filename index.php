e01532743da3fde3176d536ac1fcd39f4c6d184b
hbatur <hbatur@gmail.com>
2022-11-06 20:31:42 +0300

    v3

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "hello world! v2";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>devcloud demo</title>
+</head>
+<body>
+    <?php
+    echo "hello world! v2";
+    ?>
+</body>
+</html>
